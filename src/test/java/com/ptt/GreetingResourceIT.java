9a8371988439325496a730ddecfe23970d5f64dc
S1mple133 <pavelescu_darius@yahoo.com>
2022-07-17 13:56:39 +0200

    Removed redundant tests

--- src/test/java/com/ptt/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.ptt;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
