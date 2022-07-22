8ba435fd4b390ee7aca30a1cb64d69afd790f4b2
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-07-22 13:21:30 +0200

    added basic k8s client

--- src/main/java/com/ptt/boundary/PlanResource.java
+++ src/main/java/com/ptt/boundary/PlanResource.java
@@ -5,12 +5,16 @@ import com.ptt.control.UserRepository;
 import com.ptt.entity.Plan;
 import com.ptt.entity.User;
 import com.ptt.entity.dto.PlanDto;
+import io.fabric8.kubernetes.api.model.*;
+import io.fabric8.kubernetes.client.KubernetesClient;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.Instant;
 import java.util.List;
 
 @Path("plan")
@@ -18,7 +22,7 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 public class PlanResource {
     @ApplicationPath("/api")
-    public static class ApplicationConfiguration  extends Application {
+    public static class ApplicationConfiguration extends Application {
 
     }
 
@@ -26,6 +30,12 @@ public class PlanResource {
     PlanRepository planRepository;
     @Inject
     UserRepository userRepository;
+
+    private final KubernetesClient kubernetesClient;
+
+    public PlanResource(KubernetesClient kubernetesClient) {
+        this.kubernetesClient = kubernetesClient;
+    }
 
     @GET
     public List<PlanDto> getAllPlans() {
@@ -35,13 +45,58 @@ public class PlanResource {
     @GET
     @Path("user/{userId}")
     public List<PlanDto> getAllPlansForUser(@PathParam("userId") long userId) {
-        return planRepository.find("plan.user.id",userId).project(PlanDto.class).list();
+        return planRepository.find("plan.user.id", userId).project(PlanDto.class).list();
+    }
+
+    /*
+    apiVersion: v1
+    kind: Pod
+    metadata:
+      name: ptt-client-test
+      namespace: ptt
+    spec:
+      containers:
+      - name: ptt-client-test
+        image: ghcr.io/2023-da-ptt/ptt-client:latest
+      imagePullSecrets:
+      - name: dockerconfigjson-github-com
+     */
+    @GET
+    @Path("user/{userId}/run")
+    public Response runTestPlan(@PathParam("userId") long userId) {
+        kubernetesClient.pods().create(
+                new PodBuilder()
+                        .withApiVersion("v1")
+                        .withKind("Pod")
+                        .withMetadata(
+                                new ObjectMetaBuilder()
+                                        .withName("ptt-client")
+                                        .withNamespace("ptt")
+                                        .build())
+                        .withSpec(
+                                new PodSpecBuilder()
+                                        .withContainers(
+                                                new ContainerBuilder()
+                                                        .withName("ptt-client-" + Instant.now().getEpochSecond())
+                                                        .withImage("ghcr.io/2023-da-ptt/ptt-client:latest")
+                                                        .build()
+                                        ).withImagePullSecrets(
+                                                new LocalObjectReferenceBuilder()
+                                                        .withName("docerconfigjson-github-com")
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .build()
+        );
+
+        return Response.noContent().build();
     }
 
     @GET
     @Path("{id}")
     public PlanDto getPlanById(@PathParam("id") long planId) {
-        return planRepository.find("id",planId).project(PlanDto.class).singleResult();
+        return planRepository.find("id", planId).project(PlanDto.class).singleResult();
     }
 
     @POST
