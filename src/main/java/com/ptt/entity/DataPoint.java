5c4f8f13c8d08e1a9b1fd0e4a5c36c4f096d339c
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-08-03 09:26:51 +0200

    add default constructor to entity

--- src/main/java/com/ptt/entity/DataPoint.java
+++ src/main/java/com/ptt/entity/DataPoint.java
@@ -5,22 +5,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class DataPoint {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private final long planId;
-    private final long stepId;
-    private final long startTime;
-    private final long duration;
+    private long planId;
+    private long stepId;
+    private long startTime;
+    private long duration;
 
+    @JsonCreator
     public DataPoint(@JsonProperty("planId") long planId, @JsonProperty("stepId") long stepId, @JsonProperty("startTime") long startTime, @JsonProperty("duration") long duration) {
         this.planId = planId;
         this.stepId = stepId;
         this.startTime = startTime;
         this.duration = duration;
+    }
+
+    public DataPoint() {
     }
 
     public long getPlanId() {
@@ -34,6 +39,26 @@ public class DataPoint {
     }
     public long getDuration() {
         return duration;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPlanId(long planId) {
+        this.planId = planId;
+    }
+
+    public void setStepId(long stepId) {
+        this.stepId = stepId;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
     }
 
     @Override
