23d98d9ba8195104d0c215c73c1dc60cf5e86c87
Niklas Aichinger <niklas.aichinger@dynatrace.com>
2022-08-23 14:25:09 +0200

    add save nextsteps endpoint

--- src/main/java/com/ptt/boundary/StepResource.java
+++ src/main/java/com/ptt/boundary/StepResource.java
@@ -1,9 +1,17 @@
 package com.ptt.boundary;
 
+import com.ptt.control.InputArgumentRepository;
 import com.ptt.control.NextStepRepository;
+import com.ptt.control.OutputArgumentRepository;
 import com.ptt.control.PlanRepository;
+import com.ptt.control.StepParameterRelationRepository;
 import com.ptt.control.StepRepository;
+import com.ptt.entity.InputArgument;
+import com.ptt.entity.NextStep;
+import com.ptt.entity.OutputArgument;
 import com.ptt.entity.Step;
+import com.ptt.entity.StepParameterRelation;
+import com.ptt.entity.dto.AdvancedStepParameterRelationDto;
 import com.ptt.entity.dto.NextStepWithParameterRelationDto;
 import com.ptt.entity.dto.StepDto;
 
@@ -29,6 +37,14 @@ public class StepResource {
     @Inject
     NextStepRepository nextStepRepository;
 
+    @Inject
+    StepParameterRelationRepository relationRepository;
+
+    @Inject
+    OutputArgumentRepository outputArgumentRepository;
+    @Inject
+    InputArgumentRepository inputArgumentRepository;
+
     @GET
     public List<StepDto> getAllStepsForPlan(@PathParam("planId") long planId) {
         return stepRepository.find("plan.id", planId).project(StepDto.class).list();
@@ -44,6 +60,57 @@ public class StepResource {
     @Path("{stepId}/nexts")
     public List<NextStepWithParameterRelationDto> getAllNextsByIdForPlan(@PathParam("planId") long planId, @PathParam("stepId") long stepId) {
         return nextStepRepository.getAdvancedNextSteps(planId, stepId);
+    }
+
+    @POST
+    @Path("{stepId}/nexts")
+    @Transactional
+    public Response updateAllNextsByIdForPlan(@PathParam("planId") long planId, @PathParam("stepId") long stepId, List<NextStepWithParameterRelationDto> nexts) {
+        relationRepository
+        .getEntityManager()
+        .createQuery("""
+            delete from StepParameterRelation spr
+            where spr.id in
+            (select sprsub.id from StepParameterRelation sprsub
+                where sprsub.fromArg.step.id=?1)
+        """).setParameter(1, stepId).executeUpdate();
+
+        nextStepRepository
+        .getEntityManager()
+        .createQuery("""
+            delete from NextStep ns
+            where ns.id in
+            (select nssub.id from NextStep nssub where
+                nssub.fromStep.id=?1)
+        """).setParameter(1, stepId).executeUpdate();
+
+        //POSTGRES doesnt support cross join delete:
+        //relationRepository.delete("fromArg.step.plan.id=?1", planId);
+        //nextStepRepository.delete("delete from NextStep where fromStep.plan.id = ?1", planId);
+        Step step = stepRepository.findById(stepId);
+        for (NextStepWithParameterRelationDto nextDto : nexts) {
+            Step toStep = stepRepository.findById(nextDto.getToStep().getId());
+            if(toStep == null) {
+                return Response.status(400).build();
+            }
+            NextStep next = new NextStep();
+            next.fromStep = step;
+            next.toStep = toStep;
+            next.repeatAmount = nextDto.getRepeatAmount();
+            nextStepRepository.persist(next);
+            for(AdvancedStepParameterRelationDto relationDto : nextDto.getStepParameterRelations()) {
+                OutputArgument outArg = outputArgumentRepository.findById(relationDto.getOutputArgId());
+                InputArgument inArg = inputArgumentRepository.findById(relationDto.getInputArg().getId());
+                if(inArg ==null || outArg == null) {
+                    return Response.status(400).build();
+                }
+                StepParameterRelation relation = new StepParameterRelation();
+                relation.fromArg = outArg;
+                relation.toArg = inArg;
+                relationRepository.persist(relation);
+            }
+        }
+        return Response.ok().build();
     }
 
     @DELETE
